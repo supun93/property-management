843255db70d522b7ac2f02b42ab66a3be197567b
supun93 <69512139+supun93@users.noreply.github.com>
2025-07-13 13:29:14 +0530

    Update InvoiceController.php

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -123,11 +123,11 @@ class InvoiceController extends BaseController
         if ($this->history) {
             $this->repository->setTableTitle("Invoices History")
                 ->disableViewData("view", 'trash', 'trashList', 'add')
-                ->enableViewData("export", 'edit');
+                ->enableViewData("export", 'edit', 'download');
         } else {
             $this->repository->setTableTitle("Pending Invoices")
                 ->disableViewData("view", 'trash', 'trashList', 'add')
-                ->enableViewData("export", 'edit');
+                ->enableViewData("export", 'edit', 'download');
         }
 
 
